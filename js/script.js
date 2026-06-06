89d241d41279e8e39fef22a13b5b08ad8c6cebeb
muso-lini <mishalulchuk09@gmail.com>
2026-06-07 00:52:51 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -11,16 +11,9 @@ burgerBtn.classList.toggle("active")
 
 document.addEventListener("click", function(event) {
     const target = event.target;
-    
-    // Проверяем, был ли клик по кнопке бургера
     const isClickInsideBurger = burgerBtn.contains(target);
-    
-    // Если кликнули КУДА УГОДНО, кроме самого бургера — закрываем меню
     if (!isClickInsideBurger) {
         navMenu.classList.remove('active');
-        
-        // Если у тебя на самой кнопке бургера тоже переключался класс active (для анимации крестика),
-        // не забудь снять его и оттуда:
         burgerBtn.classList.remove('active'); 
     }
 });
